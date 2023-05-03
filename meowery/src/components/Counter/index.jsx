8fc4d466df1757fe9dfe72ffc48ead2779c13331
AnnaBrum <annahelenabrumark@gmail.com>
2023-05-03 15:06:57 +0200

    made counter component

---
+++ meowery/src/components/Counter/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Counter() {
+    return (
+        <div className="count-box">
+        <p>Flipped cards count:</p>
+        </div>
+    )
+}
+
+export default Counter;
